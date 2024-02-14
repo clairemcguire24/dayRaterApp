6c46ec8f439c64619d20cc0f5ac5344b52dbd312
Claire McGuire <claireviolet05@icloud.com>
2024-02-14 09:41:29 -0600

    Initial Commit

---
+++ dayRaterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dayRaterApp
+//
+//  Created by CLAIRE MCGUIRE on 2/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
